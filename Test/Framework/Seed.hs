70ef1a37a353fa6cdde704ec66946db792a80dbc
Max Bolingbroke <batterseapower@hotmail.com>
2008-08-28 22:52:26 +0100

    Add Show instance for Seed

--- Test/Framework/Seed.hs
+++ Test/Framework/Seed.hs
@@ -10,6 +10,10 @@ import Data.Char
 data Seed = FixedSeed Int
           | RandomSeed
 
+instance Show Seed where
+    show RandomSeed    = "random"
+    show (FixedSeed n) = show n
+
 instance Read Seed where
     readsPrec prec xs = if map toLower random_prefix == "random"
                         then [(RandomSeed, rest)]
