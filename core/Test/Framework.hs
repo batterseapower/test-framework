43e843cddff0fe4cffcf118c02b9bffa4c8c9b48
Max Bolingbroke <batterseapower@hotmail.com>
2010-05-06 20:36:10 +0100

    Refactor functionality not to do with the console runner out to a Common module

--- core/Test/Framework.hs
+++ core/Test/Framework.hs
@@ -5,13 +5,13 @@
 module Test.Framework (
         module Test.Framework.Core,
         module Test.Framework.Options,
-        module Test.Framework.Runners.Console,
+        module Test.Framework.Runners.Common,
         module Test.Framework.Runners.Options,
         module Test.Framework.Seed
     ) where
 
 import Test.Framework.Core (Test, TestName, testGroup, plusTestOptions)
 import Test.Framework.Options
-import Test.Framework.Runners.Console
+import Test.Framework.Runners.Common
 import Test.Framework.Runners.Options
 import Test.Framework.Seed
--- core/Test/Framework/Runners/Console.hs
+++ core/Test/Framework/Runners/Console.hs
@@ -1,26 +1,19 @@
 module Test.Framework.Runners.Console (
-        defaultMain, defaultMainWithArgs, defaultMainWithOpts
+        showRunTestsTop
     ) where
 
 import Test.Framework.Core
 import Test.Framework.Improving
-import Test.Framework.Options
 import Test.Framework.Runners.Console.Colors
 import Test.Framework.Runners.Console.ProgressBar
 import Test.Framework.Runners.Console.Statistics
 import Test.Framework.Runners.Console.Utilities
 import Test.Framework.Runners.Core
-import Test.Framework.Runners.Options
-import Test.Framework.Runners.Processors
 import Test.Framework.Runners.Statistics
 import Test.Framework.Runners.TimedConsumption
-import Test.Framework.Seed
 import Test.Framework.Utilities
 
 import System.Console.ANSI
-import System.Console.GetOpt
-import System.Environment
-import System.Exit
 import System.IO
 
 import Text.PrettyPrint.ANSI.Leijen
@@ -30,109 +23,16 @@ import Data.Monoid
 import Control.Monad
 
 
-instance Functor OptDescr where
-    fmap f (Option a b arg_descr c) = Option a b (fmap f arg_descr) c
-
-instance Functor ArgDescr where
-    fmap f (NoArg a) = NoArg (f a)
-    fmap f (ReqArg g s) = ReqArg (f . g) s
-    fmap f (OptArg g s) = OptArg (f . g) s
-
--- | @Nothing@ signifies that usage information should be displayed.
--- @Just@ simply gives us the contribution to overall options by the command line option.
-type SuppliedRunnerOptions = Maybe RunnerOptions
-
-optionsDescription :: [OptDescr SuppliedRunnerOptions]
-optionsDescription = [
-        Option [] ["help"]
-            (NoArg Nothing)
-            "show this help message"
-    ] ++ map (fmap Just) [
-        Option ['j'] ["threads"]
-            (ReqArg (\t -> mempty { ropt_threads = Just (read t) }) "NUMBER")
-            "number of threads to use to run tests",
-        Option [] ["test-seed"]
-            (ReqArg (\t -> mempty { ropt_test_options = Just (mempty { topt_seed = Just (read t) }) }) ("NUMBER|" ++ show RandomSeed))
-            "default seed for test random number generator",
-        Option ['a'] ["maximum-generated-tests"]
-            (ReqArg (\t -> mempty { ropt_test_options = Just (mempty { topt_maximum_generated_tests = Just (read t) }) }) "NUMBER")
-            "how many automated tests something like QuickCheck should try, by default",
-        Option [] ["maximum-unsuitable-generated-tests"]
-            (ReqArg (\t -> mempty { ropt_test_options = Just (mempty { topt_maximum_unsuitable_generated_tests = Just (read t) }) }) "NUMBER")
-            "how many unsuitable candidate tests something like QuickCheck should endure before giving up, by default",
-        Option ['o'] ["timeout"]
-            (ReqArg (\t -> mempty { ropt_test_options = Just (mempty { topt_timeout = Just (Just (secondsToMicroseconds (read t))) }) }) "NUMBER")
-            "how many seconds a test should be run for before giving up, by default",
-        Option [] ["no-timeout"]
-            (NoArg (mempty { ropt_test_options = Just (mempty { topt_timeout = Just Nothing }) }))
-            "specifies that tests should be run without a timeout, by default",
-        Option ['t'] ["select-tests"]
-            (ReqArg (\t -> mempty { ropt_test_patterns = Just [read t] }) "TEST-PATTERN")
-            "only tests that match at least one glob pattern given by an instance of this argument will be run"
-    ]
-
-interpretArgs :: [String] -> IO (Either String (RunnerOptions, [String]))
-interpretArgs args = do
-    prog_name <- getProgName
-    let usage_header = "Usage: " ++ prog_name ++ " [OPTIONS]"
-    
-    case getOpt Permute optionsDescription args of
-        (oas, n, []) | Just os <- sequence oas -> return $ Right (mconcat os, n)
-        (_, _, errs)                           -> return $ Left (concat errs ++ usageInfo usage_header optionsDescription)
-
-
-defaultMain :: [Test] -> IO ()
-defaultMain tests = do
-    args <- getArgs
-    defaultMainWithArgs tests args
-
-defaultMainWithArgs :: [Test] -> [String] -> IO ()
-defaultMainWithArgs tests args = do
-    interpreted_args <- interpretArgs args
-    case interpreted_args of
-        Right (ropts, [])    -> defaultMainWithOpts tests ropts
-        Right (_, leftovers) -> do
-            putStrLn $ "Could not understand these extra arguments: " ++ unwords leftovers
-            exitWith (ExitFailure 1)
-        Left error_message   -> do
-            putStrLn error_message
-            exitWith (ExitFailure 1)
-
-defaultMainWithOpts :: [Test] -> RunnerOptions -> IO ()
-defaultMainWithOpts tests ropts = hideCursorDuring $ do
-    let ropts' = completeRunnerOptions ropts
-    
-    -- Get a lazy list of the test results, as executed in parallel
-    run_tests <- runTests ropts' tests
-    
+showRunTestsTop :: TestStatistics -> [RunTest] -> IO TestStatistics
+showRunTestsTop test_statistics run_tests = hideCursorDuring $ do
     -- Show those test results to the user as we get them
-    let test_statistics = initialTestStatistics (totalRunTestsList run_tests)
     test_statistics' <- showRunTests 0 test_statistics run_tests
     
     -- Show the final statistics
     putStrLn ""
     putDoc $ showFinalTestStatistics test_statistics'
     
-    -- Set the error code depending on whether the tests succeded or not
-    exitWith $ if ts_no_failures test_statistics'
-               then ExitSuccess
-               else ExitFailure 1
-
-
-completeRunnerOptions :: RunnerOptions -> CompleteRunnerOptions
-completeRunnerOptions ro = RunnerOptions {
-            ropt_threads = K $ ropt_threads ro `orElse` processorCount,
-            ropt_test_options = K $ ropt_test_options ro `orElse` mempty,
-            ropt_test_patterns = K $ ropt_test_patterns ro `orElse` mempty
-        }
-
-
-totalRunTests :: RunTest -> TestCount
-totalRunTests (RunTest _ test_type _) = adjustTestCount test_type 1 mempty
-totalRunTests (RunTestGroup _ tests)  = totalRunTestsList tests
-
-totalRunTestsList :: [RunTest] -> TestCount
-totalRunTestsList = mconcat . map totalRunTests
+    return test_statistics'
 
 
 -- This code all /really/ sucks.  There must be a better way to seperate out the console-updating
