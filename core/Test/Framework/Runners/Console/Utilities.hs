50fcd0dde8c59364bb45a91dd417462515dc910d
Max Bolingbroke <batterseapower@hotmail.com>
2010-10-02 12:02:44 -0400

    Flush after showing cursor to make sure that it is restored (thanks to Edward Z Yang for the report)

--- core/Test/Framework/Runners/Console/Utilities.hs
+++ core/Test/Framework/Runners/Console/Utilities.hs
@@ -3,9 +3,10 @@ module Test.Framework.Runners.Console.Utilities (
     ) where
 
 import System.Console.ANSI
+import System.IO
 
 import Control.Exception.Extensible
 
 
 hideCursorDuring :: IO a -> IO a
-hideCursorDuring action = bracket hideCursor (const showCursor) (const action)
+hideCursorDuring action = bracket hideCursor (const (showCursor >> hFlush stdout)) (const action)
